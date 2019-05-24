a7d1bd382a2afd7a7e8284974d54feca4ec2bec0
Prateek Jain <prateek.jain2903@gmail.com>
2019-05-25 02:31:00 +0530

    Update config file

--- config.py
+++ config.py
@@ -3,11 +3,14 @@
 
 # In[ ]:
 
+import sys
+from datetime import timedelta
 
 class Config():
     def __init__(self):
         self.clean_descriptions = True
         self.batch_size = 32
+        self.num_epochs = 50
         self.embedding_size = 512
         self.activation = 'relu'
         self.regularizer = 'l2'
@@ -15,12 +18,22 @@ class Config():
         self.rnn_type = 'lstm'
         self.bidirectional_rnn = False
         self.rnn_layers = 2
+        self.image_input_shape = (299,299,3)
         self.rnn_output_size=512
+        self.word_vector_init = None
+        self.lr_reduce_factor = 0.7
+        self.lr_reduce_patience = 4
+        self.early_stopping_patience = sys.maxsize
+        self.time_limit = timedelta(hours=24)
+        self.image_augmentation=False
+        
+    def init_vocab_size(self, vocab_size):
+        self.vocab_size = vocab_size
 
 # In[ ]:
 
 
-active_config = Config()
+# active_config = Config()
 
 
 
